ecdc0985c18d85174e1ef5276f3fd32c0cea5ff1
Kateřina Holasová <kataholasova@gmail.com>
2025-04-28 16:37:41 +0200

    Generate layouts lib

---
+++ libs/layouts/src/lib/layouts.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const StyledLayouts = styled.div`
+  color: pink;
+`;
+export function Layouts() {
+  return (
+    <StyledLayouts>
+      <h1>Welcome to Layouts!</h1>
+    </StyledLayouts>
+  );
+}
+
+export default Layouts;
