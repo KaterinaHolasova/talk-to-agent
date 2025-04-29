3e44be408ea905a6427104fc8b6775604780eef8
Kateřina Holasová <kataholasova@gmail.com>
2025-04-29 18:08:52 +0200

    Move MainLayout to ui lib

--- apps/talk-to-agent/app/layouts/MainLayout.tsx
+++ apps/talk-to-agent/app/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { MainLayout as Layout } from '@talk-to-agent/layouts';
+import { MainLayout as Layout } from '@talk-to-agent/ui';
 import { Outlet } from 'react-router';
 
 export default function MainLayout() {
