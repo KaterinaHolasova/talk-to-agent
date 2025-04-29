6e9284412efffea331e8b14bea45f655e1fbd5ac
Kateřina Holasová <kataholasova@gmail.com>
2025-04-29 18:31:58 +0200

    Generate store lib

---
+++ libs/store/src/lib/store.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const StyledStore = styled.div`
+  color: pink;
+`;
+export function Store() {
+  return (
+    <StyledStore>
+      <h1>Welcome to Store!</h1>
+    </StyledStore>
+  );
+}
+
+export default Store;
