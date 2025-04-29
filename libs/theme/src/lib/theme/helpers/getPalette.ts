2dd5733859e296bfef4372382a858ae26d5bd291
Kateřina Holasová <kataholasova@gmail.com>
2025-04-29 15:46:46 +0200

    Add COLOR_PALETTE

--- libs/theme/src/lib/theme/helpers/getPalette.ts
+++ libs/theme/src/lib/theme/helpers/getPalette.ts
@@ -1,21 +1,28 @@
-import { type ThemeOptions } from '@mui/material';
+import { alpha, type ThemeOptions } from '@mui/material';
+
+const COLOR_PALETTE = {
+  primary: {
+    main: '#DD86DF',
+    dark: '#D157D3',
+    contrastText: '#1F1B20',
+  },
+  text: {
+    primary: '#4F1650',
+  },
+  background: {
+    default: '#F9EBFA',
+    paper: '#F4D6F5',
+  },
+};
 
 export function getPalette(): ThemeOptions['palette'] {
   return {
-    primary: {
-      main: '#DD86DF',
-      dark: '#D157D3',
-      contrastText: '#1F1B20',
-    },
-    text: {
-      primary: '#4F1650',
-    },
-    background: {
-      default: '#F9EBFA',
-      paper: '#F4D6F5',
-    },
+    primary: COLOR_PALETTE.primary,
+    text: COLOR_PALETTE.text,
+    background: COLOR_PALETTE.background,
     action: {
-      active: '#4F1650',
+      active: COLOR_PALETTE.text.primary,
+      hover: alpha(COLOR_PALETTE.text.primary, 0.08),
     },
   };
 }
