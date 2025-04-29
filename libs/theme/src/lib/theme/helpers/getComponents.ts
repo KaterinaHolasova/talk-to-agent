4a4e5d50da932f501d3af5d49d2cf1dc0246be7f
Kateřina Holasová <kataholasova@gmail.com>
2025-04-29 12:55:41 +0200

    Style AppBar

--- libs/theme/src/lib/theme/helpers/getComponents.ts
+++ libs/theme/src/lib/theme/helpers/getComponents.ts
@@ -1,13 +1,28 @@
 import { type Theme, type ThemeOptions } from '@mui/material';
 import { GilroyBold, GilroyRegular } from '@talk-to-agent/assets';
 
-export function getComponents({ spacing }: Theme): ThemeOptions['components'] {
+export function getComponents({
+  breakpoints,
+  spacing,
+}: Theme): ThemeOptions['components'] {
   return {
     MuiAppBar: {
       defaultProps: {
         color: 'transparent',
         elevation: 0,
         position: 'static',
+      },
+    },
+    MuiContainer: {
+      defaultProps: {
+        maxWidth: 'md',
+      },
+      styleOverrides: {
+        maxWidthMd: {
+          [breakpoints.up('md')]: {
+            maxWidth: '1000px',
+          },
+        },
       },
     },
     MuiCssBaseline: {
@@ -25,6 +40,29 @@ export function getComponents({ spacing }: Theme): ThemeOptions['components'] {
           src: local('Gilroy Bold'), local('Gilroy-Bold'), url(${GilroyBold}) format('truetype');
         }
       `,
+    },
+    MuiToolbar: {
+      defaultProps: {
+        disableGutters: true,
+      },
+      styleOverrides: {
+        root: {
+          paddingTop: spacing(3),
+          paddingBottom: spacing(3),
+          [breakpoints.up('sm')]: {
+            paddingTop: spacing(4),
+            paddingBottom: spacing(4),
+          },
+          [breakpoints.up('md')]: {
+            paddingTop: spacing(6),
+            paddingBottom: spacing(6),
+          },
+          [breakpoints.up('lg')]: {
+            paddingTop: spacing(9),
+            paddingBottom: spacing(9),
+          },
+        },
+      },
     },
   };
 }
