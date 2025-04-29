443d957192bb5b9bf90af181c12bd91353694e3a
Kateřina Holasová <kataholasova@gmail.com>
2025-04-29 09:32:27 +0200

    Generate assets lib

---
+++ libs/assets/src/lib/assets.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const StyledAssets = styled.div`
+  color: pink;
+`;
+export function Assets() {
+  return (
+    <StyledAssets>
+      <h1>Welcome to Assets!</h1>
+    </StyledAssets>
+  );
+}
+
+export default Assets;
