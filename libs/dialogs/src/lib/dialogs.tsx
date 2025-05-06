efa618a589aebe3e68e0251ce92eaa144bdbe65d
Kateřina Holasová <kataholasova@gmail.com>
2025-05-06 22:29:06 +0200

    Generate dialogs lib

---
+++ libs/dialogs/src/lib/dialogs.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const StyledDialogs = styled.div`
+  color: pink;
+`;
+export function Dialogs() {
+  return (
+    <StyledDialogs>
+      <h1>Welcome to Dialogs!</h1>
+    </StyledDialogs>
+  );
+}
+
+export default Dialogs;
